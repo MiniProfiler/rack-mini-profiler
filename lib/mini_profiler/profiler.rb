ef7bfbb70652dd1e5e0a9d5de78a9f560fcb875d
Robin Ward <robin.ward@gmail.com>
2012-04-17 12:40:31 -0400

    Can supply position via option

--- lib/mini_profiler/profiler.rb
+++ lib/mini_profiler/profiler.rb
@@ -28,7 +28,8 @@ module Rack
       {
         :auto_inject => true, # automatically inject on every html page
         :base_url_path => "/mini-profiler-resources/",
-        :authorize_cb => lambda {|env| true} # callback returns true if this request is authorized to profile
+        :authorize_cb => lambda {|env| true}, # callback returns true if this request is authorized to profile
+        :position => 'left'  # Where it is displayed
       }
     end
 
@@ -179,7 +180,7 @@ module Rack
 			ids = "[\"%s\"]" % current['page_struct']['Id'].to_s
 			path = @options[:base_url_path]
 			version = MiniProfiler::VERSION
-			position = 'left'
+			position = @options[:position]
 			showTrivial = false
 			showChildren = false
 			maxTracesToShow = 10
