dd318b7775f35e2cf5b869a2b9b051fbdafdf10d
Roger Leite <roger.barreto@gmail.com>
2012-10-01 15:55:22 -0300

    Profile Ruby Net::HTTP if is defined
    During boot, if Net::HTTP is defined, start profile on
    request method.

---
+++ lib/patches/net_patches.rb
@@ -0,0 +1,14 @@
+if (defined?(Net) && defined?(Net::HTTP))
+
+  Net::HTTP.class_eval do
+    def request_with_mini_profiler(*args, &block)
+      request = args[0]
+      Rack::MiniProfiler.step("Net::HTTP #{request.method} #{request.path}") do
+        request_without_mini_profiler(*args, &block)
+      end
+    end
+    alias request_without_mini_profiler request
+    alias request request_with_mini_profiler
+  end
+
+end
--- lib/rack-mini-profiler.rb
+++ lib/rack-mini-profiler.rb
@@ -1,5 +1,6 @@
 require 'mini_profiler/profiler'
 require 'patches/sql_patches'
+require 'patches/net_patches'
 
 if defined?(::Rails) && ::Rails::VERSION::MAJOR.to_i >= 3
   require 'mini_profiler_rails/railtie'
