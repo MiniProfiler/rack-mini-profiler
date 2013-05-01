147a2296eb22660cf289b50868ca1c52fbd32454
Madan Thangavelu <madankumar.t@gmail.com>
2013-05-01 12:59:02 -0700

    Update profiler.rb
    Making cache headers empty strings rather than nil

--- lib/mini_profiler/profiler.rb
+++ lib/mini_profiler/profiler.rb
@@ -272,8 +272,8 @@ module Rack
 
         # Strip all the caching headers so we don't get 304s back
         #  This solves a very annoying bug where rack mini profiler never shows up
-        env['HTTP_IF_MODIFIED_SINCE'] = nil
-        env['HTTP_IF_NONE_MATCH'] = nil
+        env['HTTP_IF_MODIFIED_SINCE'] = ''
+        env['HTTP_IF_NONE_MATCH'] = ''
 
         status,headers,body = @app.call(env)
         client_settings.write!(headers)
