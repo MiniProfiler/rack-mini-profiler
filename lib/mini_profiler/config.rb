538ded477a52fc0ee9fd972296a777880fcc4dbe
eileencodes <eileencodes@gmail.com>
2013-11-17 16:26:30 -0500

    remove instance var and attr_accessor for insert_after_middlewares
    With the new insert_before ActionDispatch::Static these
    attributes/instance vars are no longer necessary

--- lib/mini_profiler/config.rb
+++ lib/mini_profiler/config.rb
@@ -15,8 +15,7 @@ module Rack
     attr_accessor :auto_inject, :base_url_path, :pre_authorize_cb, :position,
         :backtrace_remove, :backtrace_includes, :backtrace_ignores, :skip_schema_queries,
         :storage, :user_provider, :storage_instance, :storage_options, :skip_paths, :authorization_mode,
-        :toggle_shortcut, :start_hidden, :backtrace_threshold_ms, :storage_failure, :logger,
-        :insert_after_middlewares
+        :toggle_shortcut, :start_hidden, :backtrace_threshold_ms, :storage_failure, :logger
 
     # Deprecated options
     attr_accessor :use_existing_jquery
@@ -38,7 +37,6 @@ module Rack
           @toggle_shortcut = 'Alt+P'
           @start_hidden = false
           @backtrace_threshold_ms = 0
-          @insert_after_middlewares = %w{HerokuDeflater::SkipBinary Rack::Deflater}
           @storage_failure = Proc.new do |exception|
             if @logger
               @logger.warn("MiniProfiler storage failure: #{exception.message}")
