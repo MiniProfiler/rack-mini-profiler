d233d8bb3b27e89eb3cdb1c94a5ed7cd82d368c2
eileencodes <eileencodes@gmail.com>
2014-01-16 21:57:08 -0500

    remove whitesapce from SqlTimerStruct

--- lib/mini_profiler/sql_timer_struct.rb
+++ lib/mini_profiler/sql_timer_struct.rb
@@ -7,25 +7,25 @@ module Rack
     class SqlTimerStruct < TimerStruct
       def initialize(query, duration_ms, page, parent, skip_backtrace = false, full_backtrace = false)
 
-        stack_trace = nil 
+        stack_trace = nil
         unless skip_backtrace || duration_ms < Rack::MiniProfiler.config.backtrace_threshold_ms
           # Allow us to filter the stack trace
           stack_trace = ""
            # Clean up the stack trace if there are options to do so
           Kernel.caller.each do |ln|
             ln.gsub!(Rack::MiniProfiler.config.backtrace_remove, '') if Rack::MiniProfiler.config.backtrace_remove and !full_backtrace
-            if    full_backtrace or 
+            if    full_backtrace or
                   (
                     (
-                      Rack::MiniProfiler.config.backtrace_includes.nil? or 
+                      Rack::MiniProfiler.config.backtrace_includes.nil? or
                       Rack::MiniProfiler.config.backtrace_includes.all?{|regex| ln =~ regex}
                     ) and
                     (
-                      Rack::MiniProfiler.config.backtrace_ignores.nil? or 
+                      Rack::MiniProfiler.config.backtrace_ignores.nil? or
                       Rack::MiniProfiler.config.backtrace_ignores.all?{|regex| !(ln =~ regex)}
                     )
-                  ) 
-              stack_trace << ln << "\n" 
+                  )
+              stack_trace << ln << "\n"
             end
           end
         end
@@ -35,7 +35,7 @@ module Rack
 
         super("ExecuteType" => 3, # TODO
               "FormattedCommandString" => query,
-              "StackTraceSnippet" => stack_trace, 
+              "StackTraceSnippet" => stack_trace,
               "StartMilliseconds" => ((Time.now.to_f * 1000).to_i - page['Started']) - duration_ms,
               "DurationMilliseconds" => duration_ms,
               "FirstFetchDurationMilliseconds" => duration_ms,
