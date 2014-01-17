eb3298cb9c083eb72374dbfb9f87bf7c9d8fe950
eileencodes <eileencodes@gmail.com>
2014-01-16 22:39:07 -0500

    Cleanup structure using strings as symbols in CustomTimerStruct

--- lib/mini_profiler/custom_timer_struct.rb
+++ lib/mini_profiler/custom_timer_struct.rb
@@ -11,10 +11,10 @@ module Rack
         @page = page
         @type = type
 
-        super("Type" => type,
-              "StartMilliseconds" => ((Time.now.to_f * 1000).to_i - page['Started']) - duration_ms,
-              "DurationMilliseconds" => duration_ms,
-              "ParentTimingId" => nil)
+        super(:type => type,
+              :start_milliseconds => ((Time.now.to_f * 1000).to_i - page[:started]) - duration_ms,
+              :duration_milliseconds => duration_ms,
+              :parent_timing_id => nil)
       end
     end
 
