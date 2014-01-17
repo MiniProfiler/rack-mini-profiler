8c39dbcfdd841a54217565401195009f271558e0
eileencodes <eileencodes@gmail.com>
2014-01-16 22:38:48 -0500

    Cleanup structure using strings as symbols in context

--- lib/mini_profiler/context.rb
+++ lib/mini_profiler/context.rb
@@ -2,7 +2,7 @@ class Rack::MiniProfiler::Context
   attr_accessor :inject_js,:current_timer,:page_struct,:skip_backtrace,:full_backtrace,:discard, :mpt_init, :measure
 
   def initialize(opts = {})
-    opts["measure"] = true unless opts.key? "measure"
+    opts[:measure] = true unless opts.key? :measure
     opts.each do |k,v|
       self.instance_variable_set('@' + k, v)
     end
