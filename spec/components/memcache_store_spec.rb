bd24f620771940f73877ec6d93bc96e388e61a1d
eileencodes <eileencodes@gmail.com>
2014-04-15 22:39:53 -0400

    fix db store files to use symbols instead of strings

--- spec/components/memcache_store_spec.rb
+++ spec/components/memcache_store_spec.rb
@@ -16,27 +16,27 @@ describe Rack::MiniProfiler::MemcacheStore do
 
       it 'can store a PageStruct and retrieve it' do
         page_struct = Rack::MiniProfiler::PageTimerStruct.new({})
-        page_struct['Id'] = "XYZ"
-        page_struct['Random'] = "random"
+        page_struct[:id] = "XYZ"
+        page_struct[:random] = "random"
         @store.save(page_struct)
         page_struct = @store.load("XYZ")
-        page_struct['Random'].should == "random"
-        page_struct['Id'].should == "XYZ"
+        page_struct[:random].should == "random"
+        page_struct[:id].should == "XYZ"
       end
 
       it 'can list unviewed items for a user' do
-        @store.set_unviewed('a', 'XYZ')
-        @store.set_unviewed('a', 'ABC')
-        @store.get_unviewed_ids('a').length.should == 2
-        @store.get_unviewed_ids('a').include?('XYZ').should be_true
-        @store.get_unviewed_ids('a').include?('ABC').should be_true
+        @store.set_unviewed(:a, 'XYZ')
+        @store.set_unviewed(:a, 'ABC')
+        @store.get_unviewed_ids(:a).length.should == 2
+        @store.get_unviewed_ids(:a).include?('XYZ').should be_true
+        @store.get_unviewed_ids(:a).include?('ABC').should be_true
       end
 
       it 'can set an item to viewed once it is unviewed' do
-        @store.set_unviewed('a', 'XYZ')
-        @store.set_unviewed('a', 'ABC')
-        @store.set_viewed('a', 'XYZ')
-        @store.get_unviewed_ids('a').should == ['ABC']
+        @store.set_unviewed(:a, 'XYZ')
+        @store.set_unviewed(:a, 'ABC')
+        @store.set_viewed(:a, 'XYZ')
+        @store.get_unviewed_ids(:a).should == ['ABC']
       end
 
     end
