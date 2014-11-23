a04118464722a8aa570648fd095fb09d30101ef7
Roman Heinrich <roman.heinrich@gmail.com>
2014-11-23 22:10:44 +0100

    instrument coverage to ignore local .direnv folder

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,13 @@
+require 'simplecov'
+SimpleCov.start do
+  add_filter "/spec/"
+  add_filter "/.direnv/"
+end
+if ENV['CI']=='true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
 Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
