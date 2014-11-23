f2e6d582f9fd06b95a41a29532bf45ff2ceb286b
Roman Heinrich <roman.heinrich@gmail.com>
2014-11-23 21:11:03 +0100

    move require statements to the main lib file

--- lib/mini_profiler/profiler.rb
+++ lib/mini_profiler/profiler.rb
@@ -1,30 +1,5 @@
-require 'json'
-require 'timeout'
-require 'thread'
-
-require 'mini_profiler/version'
-require 'mini_profiler/page_timer_struct'
-require 'mini_profiler/sql_timer_struct'
-require 'mini_profiler/custom_timer_struct'
-require 'mini_profiler/client_timer_struct'
-require 'mini_profiler/request_timer_struct'
-require 'mini_profiler/storage/abstract_store'
-require 'mini_profiler/storage/memcache_store'
-require 'mini_profiler/storage/memory_store'
-require 'mini_profiler/storage/redis_store'
-require 'mini_profiler/storage/file_store'
-require 'mini_profiler/config'
-require 'mini_profiler/profiling_methods'
-require 'mini_profiler/context'
-require 'mini_profiler/client_settings'
-require 'mini_profiler/gc_profiler'
-# TODO
-# require 'mini_profiler/gc_profiler_ruby_head' if Gem::Version.new('2.1.0') <= Gem::Version.new(RUBY_VERSION)
-
 module Rack
-
   class MiniProfiler
-
     class << self
 
       include Rack::MiniProfiler::ProfilingMethods
@@ -567,6 +542,4 @@ module Rack
     end
 
   end
-
 end
-
--- lib/rack-mini-profiler.rb
+++ lib/rack-mini-profiler.rb
@@ -1,4 +1,27 @@
+require 'json'
+require 'timeout'
+require 'thread'
+
+require 'mini_profiler/version'
+require 'mini_profiler/page_timer_struct'
+require 'mini_profiler/sql_timer_struct'
+require 'mini_profiler/custom_timer_struct'
+require 'mini_profiler/client_timer_struct'
+require 'mini_profiler/request_timer_struct'
+require 'mini_profiler/storage/abstract_store'
+require 'mini_profiler/storage/memcache_store'
+require 'mini_profiler/storage/memory_store'
+require 'mini_profiler/storage/redis_store'
+require 'mini_profiler/storage/file_store'
+require 'mini_profiler/config'
+require 'mini_profiler/profiling_methods'
+require 'mini_profiler/context'
+require 'mini_profiler/client_settings'
+require 'mini_profiler/gc_profiler'
 require 'mini_profiler/profiler'
+# TODO
+# require 'mini_profiler/gc_profiler_ruby_head' if Gem::Version.new('2.1.0') <= Gem::Version.new(RUBY_VERSION)
+
 require 'patches/sql_patches'
 require 'patches/net_patches'
 
