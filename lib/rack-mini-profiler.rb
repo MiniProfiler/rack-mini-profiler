73527d15dda33427fc4e6dc0eabb505b2f7cc43a
Sathya Sekaran <sfsekaran@gmail.com>
2016-10-05 21:28:16 -0700

    fixes unstable dependency on ::Rails::VERSION
    Fixes #267

--- lib/rack-mini-profiler.rb
+++ lib/rack-mini-profiler.rb
@@ -31,6 +31,6 @@ require 'mini_profiler/profiler'
 require 'patches/sql_patches'
 require 'patches/net_patches'
 
-if defined?(::Rails) && ::Rails::VERSION::MAJOR.to_i >= 3
+if defined?(::Rails) && defined?(::Rails::VERSION) && ::Rails::VERSION::MAJOR.to_i >= 3
   require 'mini_profiler_rails/railtie'
 end
