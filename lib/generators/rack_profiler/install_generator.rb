66813f3a77c5395fd5648d2454d59ab41909d5cd
Yoshiyuki Hirano <yhirano@me.com>
2017-07-17 08:55:03 +0900

    Add RackProfiler::Generators::InstallGenerator

---
+++ lib/generators/rack_profiler/install_generator.rb
@@ -0,0 +1,11 @@
+module RackProfiler
+  module Generators
+    class InstallGenerator < ::Rails::Generators::Base
+      source_root File.expand_path("templates", __dir__)
+
+      def create_initializer_file
+        copy_file "rack_profiler.rb", "config/initializers/rack_profiler.rb"
+      end
+    end
+  end
+end
