b95e5ec8fcdd87f1ba11002f5a1d3b94475497c4
OsamaSayegh <asooomaasoooma90@gmail.com>
2019-10-24 04:48:38 +0000

    Don't inject polyfills

--- lib/mini_profiler/asset_version.rb
+++ lib/mini_profiler/asset_version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 module Rack
   class MiniProfiler
-    ASSET_VERSION = 'c19ec93add60279596554a2ca8be7c77'
+    ASSET_VERSION = '1b7787a08352388b35f032cdefc0860d'
   end
 end
