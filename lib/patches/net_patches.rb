06f3f96e25e4c8d8893909745e80ff1b960d982a
Sergei Smagin <smaginsergey1310@gmail.com>
2020-02-06 09:43:06 +0100

    Bring *args back to Net::HTTP patch in case more params are added down the line

--- lib/patches/net_patches.rb
+++ lib/patches/net_patches.rb
@@ -2,7 +2,7 @@
 
 if (defined?(Net) && defined?(Net::HTTP))
   module NetHTTPWithMiniProfiler
-    def request(request, body = nil, &block)
+    def request(request, *args, &block)
       Rack::MiniProfiler.step("Net::HTTP #{request.method} #{request.path}") do
         super
       end
