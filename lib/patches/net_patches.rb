2f321c1e3dbfdabb6bed2f9e3af14af2913efffb
OsamaSayegh <asooomaasoooma90@gmail.com>
2020-03-14 11:15:33 +0300

    Revert "FEATURE: Prepend Net::HTTP patch instead of class_eval and aliasing (#429)"
    This reverts commit 89cf0948fcd0dc6e6797a992e227d6250293d0a3.
    
    Sadly this causes conflicts with at least a couple of popular gems that
    have their own patches for `Net::HTTP#request`. The gems apply their
    patches by using method aliasing which conflicts with patches applied
    using module prepend and causes infinite loop.
    
    https://github.com/MiniProfiler/rack-mini-profiler/issues/437
    Ruby issue: https://bugs.ruby-lang.org/issues/11120

--- lib/patches/net_patches.rb
+++ lib/patches/net_patches.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
 if (defined?(Net) && defined?(Net::HTTP))
-  module NetHTTPWithMiniProfiler
-    def request(request, *args, &block)
+
+  Net::HTTP.class_eval do
+    def request_with_mini_profiler(*args, &block)
+      request = args[0]
       Rack::MiniProfiler.step("Net::HTTP #{request.method} #{request.path}") do
-        super
+        request_without_mini_profiler(*args, &block)
       end
     end
+    alias request_without_mini_profiler request
+    alias request request_with_mini_profiler
   end
 
-  Net::HTTP.prepend(NetHTTPWithMiniProfiler)
 end
