cc4660b35a9a5aaff4ff5bedab30a29b74b716ff
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2023-02-08 13:08:18 -0600

    Rename lib/mini_profiler/profiler.rb to lib/mini_profiler.rb
    This file contains Rack::MiniProfiler, not Rack::MiniProfiler::Profiler.

--- lib/mini_profiler.rb
+++ lib/mini_profiler.rb
@@ -27,11 +27,11 @@ module Rack
       end
 
       def resources_root
-        @resources_root ||= ::File.expand_path("../../html", __FILE__)
+        @resources_root ||= ::File.expand_path("../html", __FILE__)
       end
 
       def share_template
-        @share_template ||= ERB.new(::File.read(::File.expand_path("../html/share.html", ::File.dirname(__FILE__))))
+        @share_template ||= ERB.new(::File.read(::File.expand_path("html/share.html", ::File.dirname(__FILE__))))
       end
 
       def current
@@ -773,7 +773,7 @@ This is the help menu of the <a href='#{Rack::MiniProfiler::SOURCE_CODE_URI}'>ra
       end
 
       # TODO : cache this snippet
-      script = ::File.read(::File.expand_path('../html/profile_handler.js', ::File.dirname(__FILE__)))
+      script = ::File.read(::File.expand_path('html/profile_handler.js', ::File.dirname(__FILE__)))
       # replace the variables
       settings.each do |k, v|
         regex = Regexp.new("\\{#{k.to_s}\\}")
