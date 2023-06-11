7062c16003c3fdd194e4b20f36281d72b8de50ac
Masato Ohba <over.rye@gmail.com>
2023-06-11 23:55:20 +0900

    Use profile query parameter
    Profile parameter got config-able with https://github.com/MiniProfiler/rack-mini-profiler/pull/553, but framegraph ability has been still depending on `pp`. It should be use `@config.profile_parameter`

--- lib/mini_profiler.rb
+++ lib/mini_profiler.rb
@@ -359,7 +359,7 @@ module Rack
         # Prevent response body from being compressed
         env['HTTP_ACCEPT_ENCODING'] = 'identity' if config.suppress_encoding
 
-        if query_string =~ /pp=(async-)?flamegraph/ || env['HTTP_REFERER'] =~ /pp=async-flamegraph/
+        if query_string =~ /#{@config.profile_parameter}=(async-)?flamegraph/ || env['HTTP_REFERER'] =~ /#{@config.profile_parameter}=async-flamegraph/
           if defined?(StackProf) && StackProf.respond_to?(:run)
             # do not sully our profile with mini profiler timings
             current.measure = false
