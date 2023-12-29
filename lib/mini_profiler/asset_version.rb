a41679ef3d987f2766c4a93e04e03a15d9906e66
Nate Berkopec <nate.berkopec@gmail.com>
2023-12-29 09:52:36 +0900

    Badge: align columns, display page name

--- lib/mini_profiler/asset_version.rb
+++ lib/mini_profiler/asset_version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 module Rack
   class MiniProfiler
-    ASSET_VERSION = 'c336b1303a50b21f5222bc23262ca7bf'
+    ASSET_VERSION = '1369b689f72a69d94d8d7d0ffc2d4842'
   end
 end
