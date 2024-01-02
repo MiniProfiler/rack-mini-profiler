804f2da6ff522bc103c529a79a1a2889a692dbbc
Nate Berkopec <nate.berkopec@gmail.com>
2024-01-02 09:25:00 +0900

    Always display totals control (so URL/name is hidden)

--- lib/mini_profiler/asset_version.rb
+++ lib/mini_profiler/asset_version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 module Rack
   class MiniProfiler
-    ASSET_VERSION = '1369b689f72a69d94d8d7d0ffc2d4842'
+    ASSET_VERSION = '116e2a6fd81c286e004e2a0afb03baa1'
   end
 end
