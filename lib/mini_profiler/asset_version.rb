9e7d5630018d2e98874504f83a314276b6631db3
Cory Streiff <c.streiff@pm.me>
2025-01-29 13:22:52 +0100

    Fix alignment of timings for SQL statements

--- lib/mini_profiler/asset_version.rb
+++ lib/mini_profiler/asset_version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 module Rack
   class MiniProfiler
-    ASSET_VERSION = '116e2a6fd81c286e004e2a0afb03baa1'
+    ASSET_VERSION = '99c2328f8c8fc0277a7771020f1e59b3'
   end
 end
